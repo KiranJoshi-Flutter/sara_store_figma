85654540a0bc15da9158c438bda159d8605ebf6f
kiranjoshi <keronz7777@gmail.com>
2022-04-12 14:50:58 +0545

    login_screen 100% complete

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -59,122 +59,170 @@ class _LoginScreenState extends State<LoginScreen> {
                   top: 36.0,
                   right: 46.0,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Login',
-                      style: appTextStyle(
-                        FontWeight.w700,
-                        18.0,
-                        Colors.black,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 36.0,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          IconlyLight.message,
-                          color: Color(0xFF868686),
-                        ),
-                        SizedBox(
-                          width: 12.0,
-                        ),
-                        Text(
-                          'Email',
-                          style: appTextStyle(
-                            FontWeight.w600,
-                            16.0,
-                            Color(0xFF868686),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 4.0,
-                      ),
-                      child: TextFormField(
-                        controller: emailController,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
                         style: appTextStyle(
-                          FontWeight.w600,
-                          17.0,
+                          FontWeight.w700,
+                          18.0,
                           Colors.black,
                         ),
-                        keyboardType: TextInputType.emailAddress,
-                        cursorColor: Colors.grey,
                       ),
-                    ),
-                    SizedBox(
-                      height: 46.0,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          IconlyLight.lock,
-                          color: Color(0xFF868686),
+                      SizedBox(
+                        height: 36.0,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            IconlyLight.message,
+                            color: Color(0xFF868686),
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Text(
+                            'Email',
+                            style: appTextStyle(
+                              FontWeight.w600,
+                              16.0,
+                              Color(0xFF868686),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 4.0,
                         ),
-                        SizedBox(
-                          width: 12.0,
-                        ),
-                        Text(
-                          'Password',
+                        child: TextFormField(
+                          controller: emailController,
                           style: appTextStyle(
                             FontWeight.w600,
-                            16.0,
-                            Color(0xFF868686),
+                            17.0,
+                            Colors.black,
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          cursorColor: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 46.0,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            IconlyLight.lock,
+                            color: Color(0xFF868686),
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Text(
+                            'Password',
+                            style: appTextStyle(
+                              FontWeight.w600,
+                              16.0,
+                              Color(0xFF868686),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 4.0,
+                        ),
+                        child: TextFormField(
+                          obscuringCharacter: '*',
+                          controller: passwordController,
+                          style: appTextStyle(
+                            FontWeight.w600,
+                            17.0,
+                            Colors.black,
+                          ),
+                          obscureText: passwordHide,
+                          keyboardType: TextInputType.visiblePassword,
+                          cursorColor: Colors.grey,
+                          decoration: InputDecoration(
+                            suffixIcon: InkWell(
+                              child: Text(
+                                passwordHide ? 'Show' : 'Hide',
+                                style: appTextStyle(
+                                  FontWeight.w600,
+                                  15.0,
+                                  kSecondaryColor,
+                                ),
+                              ),
+                              onTap: () {
+                                setState(
+                                  () {
+                                    passwordHide = !passwordHide;
+                                  },
+                                );
+                              },
+                            ),
                           ),
                         ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 4.0,
                       ),
-                      child: TextFormField(
-                        obscuringCharacter: '*',
-                        controller: passwordController,
+                      SizedBox(height: 24.0),
+                      Text(
+                        'Forgot passcode?',
                         style: appTextStyle(
                           FontWeight.w600,
-                          17.0,
-                          Colors.black,
+                          16.0,
+                          kSecondaryColor,
                         ),
-                        obscureText: passwordHide,
-                        keyboardType: TextInputType.visiblePassword,
-                        cursorColor: Colors.grey,
-                        decoration: InputDecoration(
-                          suffixIcon: InkWell(
-                            child: Text(
-                              passwordHide ? 'Show' : 'Hide',
-                              style: appTextStyle(
-                                FontWeight.w600,
-                                15.0,
-                                kSecondaryColor,
+                      ),
+                      SizedBox(
+                        height: 62.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 66.0,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  primary: Color(0xFFEE5D26),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Login',
+                                  style: appTextStyle(
+                                    FontWeight.w700,
+                                    20.0,
+                                    Colors.white,
+                                  ),
+                                ),
                               ),
                             ),
-                            onTap: () {
-                              setState(
-                                () {
-                                  passwordHide = !passwordHide;
-                                },
-                              );
-                            },
                           ),
-                        ),
+                        ],
                       ),
-                    ),
-                    SizedBox(height: 24.0),
-                    Text(
-                      'Forgot passcode?',
-                      style: appTextStyle(
-                        FontWeight.w600,
-                        16.0,
-                        kSecondaryColor,
+                      SizedBox(
+                        height: 20.0,
                       ),
-                    ),
-                  ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Create account',
+                            style: appTextStyle(
+                              FontWeight.w600,
+                              16.0,
+                              Color(0xFFFEA517),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
